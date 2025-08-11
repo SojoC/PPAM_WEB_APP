070eb83ae6a6321ad53aa5427d871ea68777c467
SojoC <sojoc@gmail.com>
2025-08-11 13:11:30 -0400

    Fix: Ampliar campo password_hash para compatibilidad con PostgreSQL

--- core/models.py
+++ core/models.py
@@ -20,7 +20,7 @@ class User(UserMixin, db.Model):
     fecha_bautismo = db.Column(db.Date, nullable=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     congregacion_id = db.Column(db.Integer, db.ForeignKey('congregaciones.id'))
     privilegios = db.relationship('Privilegio', secondary=user_privilegios, backref=db.backref('users', lazy=True))
